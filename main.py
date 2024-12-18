6e977c119f8ee4062c764e95afc85d4f60680e77
Jothi <jothi/90mani@gmail.com>
2024-12-18 08:48:29 -0800

    Initial commit.Create images,columns and description

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1,col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+with col2:
+    st.title("Jothi")
+    content = """Hi,I am jothi.I am a python developer working in Tata consultancy services."""
+    st.info(content)
