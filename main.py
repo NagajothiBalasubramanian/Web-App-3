9b0ca86317fe4b31505f35f1c6e9c3171a0546c6
Jothi <jothi/90mani@gmail.com>
2024-12-18 12:58:51 -0800

    Added header in columns from data.csv file using pandas

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -10,3 +11,18 @@ with col2:
     st.title("Jothi")
     content = """Hi,I am jothi.I am a python developer working in Tata consultancy services."""
     st.info(content)
+
+st.write("Below you can add some features and description to explain the user to understand more about the app ")
+
+col3,col4 = st.columns(2)
+
+dataframe = pandas.read_csv("data.csv",sep=";")
+with col1:
+    for index,row in dataframe[:10].iterrows():
+        st.header(row["title"])
+
+with col2:
+    for index, row in dataframe[10:].iterrows():
+        st.header(row["title"])
+
+
