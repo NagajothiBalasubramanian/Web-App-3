369e7dd4381276e83de62e247bf12414edc5f8af
Jothi <jothi/90mani@gmail.com>
2024-12-19 02:26:57 -0800

    Add description,image and link

--- main.py
+++ main.py
@@ -9,20 +9,27 @@ with col1:
     st.image("images/photo.png")
 with col2:
     st.title("Jothi")
-    content = """Hi,I am jothi.I am a python developer working in Tata consultancy services."""
+    content = """Hi,I am Jothi.I am a python developer working in Tata consultancy services."""
     st.info(content)
 
 st.write("Below you can add some features and description to explain the user to understand more about the app ")
 
-col3,col4 = st.columns(2)
+col3,empty_col,col4 = st.columns([1.5, 0.5, 1.5])
 
 dataframe = pandas.read_csv("data.csv",sep=";")
-with col1:
+
+with col3:
     for index,row in dataframe[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[sourcecode]({row['url']})")
 
-with col2:
+with col4:
     for index, row in dataframe[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" +row["image"])
+        st.write(f"[sourcecode]({row['url']})")
 
 
